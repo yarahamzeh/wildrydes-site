1d7f0fc61eeb4087b7a2e95a67bf9fa9a266d3d6
yarahamzeh <yarahamzeh@yahoo.com>
2025-05-04 21:19:35 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_14ITHBnNV', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '8km8cn620veqenlohgbtf7g3g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_q40e6zalA', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2efqt6ukhf3595te2rgb2t9eec', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
